03eee6dcd7e0745d0e1752603b3d9d71ecd721b9
Rihan Arfan <me@file.properties>
2025-02-10 04:35:35 +0000

    Initial commit

---
+++ index.ts
@@ -0,0 +1,13 @@
+export interface Env {
+  ENVIRONMENT: string;
+}
+
+export default {
+  async fetch(request: Request, env: Env): Promise<Response> {
+    return new Response(env.ENVIRONMENT, {
+      headers: {
+        'content-type': 'text/plain',
+      },
+    });
+  }
+};
